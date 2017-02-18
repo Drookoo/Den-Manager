e54f253139627b532465ce9eb52bd40e7c50a532
Drew Ku <andrewku123@gmail.com>
2017-02-18 13:32:58 -0500

    1:32

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import smtplib
-import json
 import time
 from datetime import datetime
+from tkinter import messagebox
+from tkinter import *
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly
+from plotly.graph_objs import *
+import plotly.plotly as py
 
 #Overcrowd          Max is 675
 Max = 675
@@ -27,6 +32,31 @@ while True:
     soup = BeautifulSoup(html, "html.parser")
     current = int("".join(soup.find("div", {"id": "currentcount"}).strings))
 
+    # GUI
+    top = Tk()
+    top.geometry("300x300")
+    def helloCallBack():
+        msg = messagebox.showinfo("Current Number of People", "There are currently " + str(current) + " people aat HackNYU")
+    B = Button(top, text="Current Number of People", command=helloCallBack)
+    B.place(x=50, y=50)
+
+
+    if Max > 1:
+        top.mainloop()
+
+        plotly.tools.set_credentials_file(
+            username='nachozombie',
+            api_key='ajvmmn1scx'
+        )
+        gaussian_numbers = np.random.randn(1000)
+        plt.hist(gaussian_numbers)
+        plt.title("Gaussian Histogram")
+        plt.xlabel("Value")
+        plt.ylabel("Frequency")
+
+        fig = plt.gcf()
+
+        plot_url = py.plot_mpl(fig, filename='mpl-basic-histogram')
     with open("data.txt", "a") as myfile:
         myfile.write("TimeStamp: " + str(datetime.now().time())+"\t" + "Person Count:" + str(current) + "\n")
 
