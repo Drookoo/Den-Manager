c3582d47c036c9779edbeca052e35b708aeeba34
Drew Ku <andrewku123@gmail.com>
2017-02-19 01:19:52 -0500

    Refactored unused

--- run.py
+++ run.py
@@ -2,11 +2,6 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import time
 from datetime import datetime
-import matplotlib.pyplot as plt
-import numpy as np
-import plotly
-from plotly.graph_objs import *
-import plotly.plotly as py
 
 #Overcrowd          Max is 675
 Binary = 1
@@ -31,16 +26,8 @@ while True:
     soup = BeautifulSoup(html, "html.parser")
     current = int("".join(soup.find("div", {"id": "currentcount"}).strings))
 
-    # GUI
-    #top = Tk()
-    #top.geometry("300x300")
-    #def helloCallBack():
-        #msg = messagebox.showinfo("Current Number of People", "There are currently " + str(current) + " people aat HackNYU")
-    #B = Button(top, text="Current Number of People", command=helloCallBack)
-    #B.place(x=50, y=50)
-    #if Binary > 0:
-        #top.mainloop()
-    with open("data.txt", "a") as myfile:
+
+    with open("13hrs", "a") as myfile:
         myfile.write("TimeStamp: " + str(datetime.now().time())+"\t" + "Person Count:" + str(current) + "\n")
     Binary = 0
     time.sleep(60)
