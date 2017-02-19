d7c972f2f77d0d38f051b0c31463a8bc333e6265
Drew Ku <andrewku123@gmail.com>
2017-02-19 16:22:37 -0500

    hjkl

--- run.py
+++ run.py
@@ -31,30 +31,30 @@ while True:
         newlines = [s for s in lines if len(s) > 3]
         newnewlines = [s for s in newlines if len(s) < 16]
 
-        Nulines = ([s.strip(' \nTimeStamp ') for s in newnewlines])
-        Nulines.pop(0)
+        #Nulines = ([s.strip(' \nTimeStamp ') for s in newnewlines])
+        #Nulines.pop(0)
 
-        Nulines = list(map(int, Nulines))
-        Nulines = [x + 94 for x in Nulines]
+        #Nulines = list(map(int, Nulines))
+        #Nulines = [x + 94 for x in Nulines]
         s1 = [];
         number = 0
 
-        while number <= len(Nulines):
-            number = number + 1
-            s1.append(number)
-        new1 = (Nulines[len(Nulines) - 1])
-        new2 = (Nulines[len(Nulines)- 6])
+        #while number <= len(Nulines):
+            #number = number + 1
+            #s1.append(number)
+        #new1 = (Nulines[len(Nulines) - 1])
+        #new2 = (Nulines[len(Nulines)- 6])
 
-        x1 = int(new1) - 1
-        x2 = int(new2) - 1
-        y1 = Nulines[int(new1) - 1]
-        y2 = Nulines[int(new2) - 1]
+        #x1 = int(new1) - 1
+        #x2 = int(new2) - 1
+        #y1 = Nulines[int(new1) - 1]
+        #y2 = Nulines[int(new2) - 1]
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login("andrewku123@gmail.com", "nach0zombie")
+        server.login("andrewku123@gmail.com", " ")
 
-        msg = "Your venue is at " + oldPC + "% capacity. Your venue will close in "+ str(y2 - y1) + " Minutes"
+        msg = "Your venue is at " + oldPC + "% capacity."
         server.sendmail("andrewku123@gmail.com", "andrewku123@gmail.com", msg)
         server.quit()
 
